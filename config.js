719d1f81a186d0677f114c8f80492514ba559f19
Jieun0820 <37438852+Jieun0820@users.noreply.github.com>
2018-12-20 09:27:15 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 var config = {
   mail: {
     user : 'araka_672@naver.com', //e-mail address
-    password: 'thddbgud1225', //e-maill pw
+    password: '', //e-maill pw
     host: 'smtp.naver.com', //SMTP server
     port: '465', //SMTP port
     expiresIn: 60*5
